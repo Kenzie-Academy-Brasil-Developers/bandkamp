2947869ab83bfc0c3ac3a2c1137ed113c13e4ca5
iasmim <iasmimstuff@gmail.com>
2022-08-31 19:35:03 -0300

    feat: api docs

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'musicians',
     'albums',
     'songs',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
